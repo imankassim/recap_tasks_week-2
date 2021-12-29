285e017f3ba763ac37914ccc7a00a64847718ddc
Iman Kassim <iman.n.s.kassim@gmail.com>
2021-12-29 19:02:22 +0000

    task 2 done

--- task-2/main.js
+++ task-2/main.js
@@ -1,1 +1,30 @@
+// use setInterval on the <p> tag to increment every second
+// clear interval after 12 seconds
+
+let counter = 0;
+
+
+let counterloc = document.querySelector("p")
+
+
+function calculateCount(){
+    if (counter < 13){
+        counterloc.innerText = counter
+        counter += 1
+    } else {
+        clearInterval(timerset)
+    }
+    
+}
+
+let timerset = setInterval(calculateCount, 1000)
+
+timerset
+
+console.log(timerset)
+
+
+
+
+//clearInterval(timerset)
 
