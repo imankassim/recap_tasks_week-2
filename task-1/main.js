4a413d860260378ecf2197a5aa9773184690113a
Iman Kassim <iman.n.s.kassim@gmail.com>
2021-12-29 18:34:28 +0000

    task 1 complete

--- task-1/main.js
+++ task-1/main.js
@@ -10,3 +10,26 @@ const celebs = [
   "Richard Osman",
   "Emma Thompson"
 ];
+
+
+function makeLegend(name){
+  //console.log(name + " is now a legend.")
+  return (name + " is now a legend.")
+}
+
+
+
+let legendaryCelebs = []
+for (let i = 0; i<celebs.length; i++){
+  legendaryCelebs.push(makeLegend(celebs[i]))
+}
+
+
+console.log(legendaryCelebs)
+
+
+let vowelCelebs = celebs.filter((item) => {
+  return (item.startsWith("A") || item.startsWith("E") || item.startsWith("I") || item.startsWith("O") || item.startsWith("U"))
+})
+
+console.log(vowelCelebs)
